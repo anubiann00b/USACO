376743d9b424e82d39f0f85d84f6e5ff9554e9dd
Shreyas <skraman1999@gmail.com>
2014-12-13 18:16:01 -0800

    Replaces with simple solution.

--- src/fourteen/december/Learning.java
+++ src/fourteen/december/Learning.java
@@ -19,68 +19,61 @@ public class Learning {
         int a = Integer.valueOf(nums[1]);
         int b = Integer.valueOf(nums[2]);
         
-        Queue<Integer> spotted = new PriorityQueue<Integer>();
-        Queue<Integer> notSpotted = new PriorityQueue<Integer>();
+        Queue<Pair> cows = new PriorityQueue<Pair>();
         
         for (int i=0;i<n;i++) {
             String line = reader.readLine();
-            if (line.startsWith("NS"))
-                notSpotted.offer(Integer.parseInt(line.substring(line.indexOf(' ')+1)));
-            else
-                spotted.offer(Integer.parseInt(line.substring(line.indexOf(' ')+1)));
+            cows.offer(new Pair(Integer.parseInt(line.substring(line.indexOf(' ')+1)), !line.startsWith("NS")));
         }
         
-        boolean done = false;
         int spottedCounter = 0;
-        boolean onSpotted = spotted.peek() > notSpotted.peek();
-        int lastNotSpotted = notSpotted.peek();
-        int lastSpotted = spotted.peek();
-        int val = 0;
-        
-        outside:
-        while (!done) {
-            if (onSpotted) { // lastVal is the last spotted one, val is the next ns one
-                do {
-                    if (notSpotted.peek() != null) {
-                        val = notSpotted.poll();
-                    } else {
-                        done = true; // We're done but TODO ADD STUFF HERE
-                        break;
-                    }
-                } while (lastSpotted > val);
-                
-                // TODO ADD CASE WHERE BOTH BOUNDS ARE INSIDE
-                // val > lastVal
-                int range = (val + lastSpotted)/2 + 1;
-                if (a > val) // If the lower bound is above what we're determining
-                    spottedCounter += 0;
-                else if (b < lastSpotted) // If the upper bound is below what we're determining
-                    break;           // We're done
-                else if (a > lastSpotted)
-                    spottedCounter += Math.min(range, val - a); // If we're in the middle of the lower bound
-                else if (b < val)
-                    spottedCounter += Math.min(range, b - lastSpotted);
-                else
-                    spottedCounter += range; // Average of spotted and newSpotted over newIndex
-                
-                onSpotted = false;
-                lastNotSpotted = val;
-            } else {
-                do {
-                    if (notSpotted.peek() != null) {
-                        val = spotted.poll();
-                    } else {
-                        done = true; // We're done but TODO ADD STUFF HERE
-                        break;
-                    }
-                } while (lastNotSpotted > val);
-                onSpotted = true;
-                lastSpotted = val;
+        Pair last = cows.poll();
+        Pair next = cows.poll();
+        for (int i=a;i<=b;i++) { // last < next
+            if (i<last.pos) {
+                spottedCounter += last.spotted ? 1 : 0;
+            } else if (i>next.pos) {
+                last = next;
+                next = cows.poll();
+                if (next == null) {
+                    spottedCounter += b-i;
+                    break;
+                }
             }
+            if (last.spotted == next.spotted)
+                spottedCounter += last.spotted ? 1 : 0;
+            else if (Math.abs(i - last.pos) == Math.abs(next.pos - i))
+                spottedCounter += (last.spotted || next.spotted) ? 1 : 0;
+            else if (Math.abs(i - last.pos) < Math.abs(next.pos - i))
+                spottedCounter += last.spotted ? 1 : 0;
+            else
+                spottedCounter += next.spotted ? 1 : 0;
+                
         }
         
         BufferedWriter writer = new BufferedWriter(new FileWriter("learning.out"));
         writer.write(String.valueOf(spottedCounter));
         writer.flush();
+    }
+}
+
+class Pair implements Comparable<Pair> {
+    
+    int pos;
+    boolean spotted;
+    
+    Pair(int p, boolean s) {
+        pos = p;
+        spotted = s;
+    }
+
+    @Override
+    public int compareTo(Pair pair) {
+        return Integer.compare(this.pos, pair.pos);
+    }
+    
+    @Override
+    public String toString() {
+        return pos + ": " + (spotted?"S":"NS");
     }
 }
