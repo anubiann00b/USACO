1a05da2ce0d914adec37e5bf08b6ca2ce9771a47
Shreyas <skraman1999@gmail.com>
2015-02-23 23:08:21 -0800

    Fixed solution... AFTER TIME OMG.

--- src/y2015/m2/Censoring.java
+++ src/y2015/m2/Censoring.java
@@ -1,5 +1,3 @@
-package y2015.m2;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -8,23 +6,23 @@ import java.io.IOException;
 
 public class Censoring {
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         BufferedReader reader = new BufferedReader(new FileReader("censor.in"));
         
-        String str = reader.readLine();
+        StringBuilder str = new StringBuilder(reader.readLine());
         String censor = reader.readLine();
         
-        for (int i=0;i<str.length()-censor.length();i++) {
-            if (!str.regionMatches(i, censor, 0, censor.length()))
-                continue;
-            str = str.substring(0, i).concat(str.substring(i+censor.length(), str.length()));
-            i -= censor.length()+1;
-            if (i < 0)
-                i = 0;
+        int pos = str.indexOf(censor);
+        int lastPos = pos-censor.length()+1;
+        
+        while (pos != -1) {
+            str.delete(pos, pos+censor.length());
+            pos = str.indexOf(censor, lastPos);
+            lastPos = pos-censor.length()+1;
         }
         
         BufferedWriter writer = new BufferedWriter(new FileWriter("censor.out"));
-        writer.write(str);
+        writer.write(str.toString());
         writer.flush();
     }
 }
