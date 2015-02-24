f103bad1717f41ef2dcda3fde8db06797a9703ec
Shreyas <skraman1999@gmail.com>
2015-02-23 21:09:43 -0800

    Another attempt, but timing out.

--- src/y2015/m2/Censoring.java
+++ src/y2015/m2/Censoring.java
@@ -5,25 +5,32 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.regex.Matcher;
 
 public class Censoring {
     
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("censor.in"));
         
-        String newStr = reader.readLine();
-        String censor = Matcher.quoteReplacement(reader.readLine());
+        String str = reader.readLine();
+        String censor = reader.readLine();
         
-        String str = null;
-        
-        while (!newStr.equals(str)) {
-            str = newStr;
-            newStr = str.replaceFirst(censor, "");
+        for (int i=0;i<str.length();i++) {
+            for (int j=0;j<censor.length();j++) {
+                if (str.length() <= i+j)
+                    break;
+                if (str.charAt(i+j) != censor.charAt(j))
+                    break;
+                if (j == censor.length()-1) {
+                    str = str.substring(0, i).concat(str.substring(i+censor.length(), str.length()));
+                    i -= censor.length();
+                    if (i < 0)
+                        i = 0;
+                }
+            }
         }
         
         BufferedWriter writer = new BufferedWriter(new FileWriter("censor.out"));
-        writer.write(newStr);
+        writer.write(str);
         writer.flush();
     }
 }
